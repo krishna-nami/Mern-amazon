5aa09038f7315a037ccdd16ce202f3f565c64e7f
Krishna-Nami <kissb032@gmail.com>
2022-05-17 23:30:45 +1000

    Node server created

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send(data.porducts);
+});
+
+app.listen(PORT, () => {
+  console.log(` The server is running in the ${PORT} address`);
+});
