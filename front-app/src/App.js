e8f02befa1e87d22172c6af6fbadb82ee07a35db
Krishna-Nami <kissb032@gmail.com>
2022-05-20 14:30:37 +1000

    implemented react bootstrap.

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,21 +1,35 @@
 //import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import Product from './Components/Product';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">amazona</Link>
+      <div className="d-flex flex-column nav-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<Product />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<Product />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center"> @ All Right Reservd</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
