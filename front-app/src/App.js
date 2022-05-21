ce89c6da590b2fbd17f284881206f68d0971e4be
Krishna-Nami <kissb032@gmail.com>
2022-05-21 14:26:02 +1000

    intrduced Card Functionlaties

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -5,8 +5,15 @@ import Product from './screen-Components/Product';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <div className="d-flex flex-column nav-container">
       <header>
@@ -15,6 +22,17 @@ function App() {
             <LinkContainer to="/">
               <Navbar.Brand>amazona</Navbar.Brand>
             </LinkContainer>
+
+            <Nav className="me-auto">
+              <Link to="/cart" className="nav-link">
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <Badge pill bg="danger">
+                    {cart.cartItems.length}
+                  </Badge>
+                )}
+              </Link>
+            </Nav>
           </Container>
         </Navbar>
       </header>
