ac4e1cd090351736fe3b27892b68b0c14d758914
Krishna-Nami <kissb032@gmail.com>
2022-06-11 10:10:26 +1000

    Profile scren back and frontend completed

--- backend/utils.js
+++ backend/utils.js
@@ -26,6 +26,6 @@ export const isAuth = (req, res, next) => {
       }
     });
   } else {
-    res.status(401).send({ message: 'No Token' });
+    res.status(401).send({ message: 'No Token found' });
   }
 };
---
+++ front-app/src/screen-Components/OrderHistoryScreen.js
@@ -0,0 +1,95 @@
+import Axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import Button from 'react-bootstrap/Button';
+import { Store } from '../Store';
+import { getError } from '../util';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, orders: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+export default function OrderHistoryScreen() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  const navigate = useNavigate();
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const { data } = await Axios.get(`/api/orders/mine`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+  return (
+    <div>
+      <Helmet>
+        <title>Order History</title>
+      </Helmet>
+      <h2>Order History</h2>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <table className="table responsive">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>TOTAL</th>
+              <th>PAID At</th>
+              <th>DELIVRED TIME</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.totalPrice.toFixed(2)} </td>
+                <td>{order.isPaid ? order.paidAt.substring(0, 10) : 'No'}</td>
+                <td>
+                  {order.isDelivered
+                    ? order.deliveredAt.substring(0, 10)
+                    : 'Pending'}
+                </td>
+                <td>
+                  <Button
+                    type="button"
+                    variant="primary"
+                    onClick={() => {
+                      navigate(`/order/${order._id}`);
+                    }}
+                  >
+                    Details
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
